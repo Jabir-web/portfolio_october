6b43789ee563cf41c78ad9693103901c534f19db
Jabir Faisal <89572534+Jabir-web@users.noreply.github.com>
2025-11-08 22:45:39 +0500

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,7 +10,7 @@
                         <img src="{{ asset('images/jabir-s.jpg') }}" class="w-100 rounded" alt="">
                     </div>
                     <div class=" p-4 col-md-8">
-                        <h1>Jabir Faisal</h1>
+                        <h1 class="font-weight-bold">Jabir Faisal</h1>
                         <span class="mb-4 text-center text-primary head-para font-weight-bold"><i>Creative Software Developer & Designer</i></span>
                         <p>I’m passionate about crafting visually stunning, high-performing web experiences. With a focus on
                             <strong>Laravel, JavaScript, and modern UI/UX</strong> design, I turn ideas into powerful
@@ -18,13 +18,15 @@
                         </p>
                         <h6>👉 Nationality: <b class="text-primary">Pakistani</b></h6>
                         <h6>👉 Date Of Birth: <b class="text-primary">02-June-2001</b></h6>
-                        <h5 class="fw-bold mt-3">EDUCATION:</h5>
-                        <span>👉 (BSIT) Bachelour In Information Technology From
+                        <h5 class="font-weight-bold mt-3">EDUCATION:</h5>
+                        <span>👉 (BSIT) Bachelor In Information Technology From
                             <strong>Virtual University Of Pakistan</strong>.
                         </span><br>
                         <span>👉 Matriculation From
                             <strong>Jetpur Memon Academy</strong>.
                         </span>
+                      
+                        </div>
                     </div>
                 </div>
             </div>
