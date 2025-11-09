fc449a2d21d947c910e2b7990a9be15929681120
Jabir Faisal <89572534+Jabir-web@users.noreply.github.com>
2025-11-09 15:20:34 +0500

    Update cookie-banner.blade.php

--- resources/views/components/cookie-banner.blade.php
+++ resources/views/components/cookie-banner.blade.php
@@ -2,7 +2,7 @@
 @if(!Cookie::get('accept_cookies'))
 <div id="cookie-banner" style="position: fixed; bottom:0; left:0; right:0; background:#111; color:#fff; padding:15px; text-align:center; z-index:9999;">
     We use cookies to improve your experience on our website. By continuing, you agree to our use of cookies. 
-    <button id="accept-cookies" style="background:#00ffc6; border:none; padding:5px 15px; cursor:pointer;">Accept</button>
+    <button id="accept-cookies" style="background:#dd4a48; border:none; padding:5px 15px; cursor:pointer;">Accept</button>
 </div>
 
 <script>
