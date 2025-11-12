da95900c1de3011e5a52f102468de96783a3d284
Jabir Faisal <89572534+Jabir-web@users.noreply.github.com>
2025-11-13 00:20:28 +0500

    download link

--- resources/views/code-project-detail.blade.php
+++ resources/views/code-project-detail.blade.php
@@ -6,49 +6,58 @@
                 <div class="d-inline-block border-2 p-4">
 
                     <div class="row">
-                        
+
                         <div class="col-md-4">
                             <img src="{{ asset($project->image) }}" class="rounded img-thumbnail" width="100%" alt="">
 
                         </div>
                         <div class="col-md-7 p-3 d-flex flex-column justify-content-center">
-                                <div class=" rounded-circle heart-icon  d-inline-block d-flex justify-content-center align-items-center"
-                                                style="width: 50px; height: 50px;">
-                                                <i class="icon icon-heart "></i>
-                                            </div>
+                            <div class=" rounded-circle heart-icon  d-inline-block d-flex justify-content-center align-items-center"
+                                style="width: 50px; height: 50px;">
+                                <i class="icon icon-heart "></i>
+                            </div>
                             <h1 class="font-weight-bold mb-3">{{ $project->title }}</h1>
                             <p class="text-capitalize mb-3">{{ $project->level }} Level</p>
                             <div class="row ">
                                 <div class="view-box bg-white rounded p-3 w-25 col-md-3 m-1" style="font-size: 18px">
-                                    Likes: 
-                                    <span class="font-weight-bold">{{ \App\Models\Like::where('project_id', $project->id)->count() }}</span>
+                                    Likes:
+                                    <span
+                                        class="font-weight-bold">{{ \App\Models\Like::where('project_id', $project->id)->count() }}</span>
                                 </div>
                                 <div class="view-box bg-white rounded p-3 w-25 col-md-3 m-1" style="font-size: 18px">
-                                    Views: 
+                                    Views:
                                     <span class="font-weight-bold">{{ $project->views }}</span>
                                 </div>
                                 <div class="view-box bg-white rounded p-3 w-25 col-md-3 m-1" style="font-size: 18px">
-                                    Size: 
+                                    Size:
                                     <span class="font-weight-bold">{{ $project->size }}</span>
                                 </div>
                                 <div class="view-box bg-white rounded p-3 w-25 col-md-3 m-1" style="font-size: 18px">
-                                    Version: 
-                                    <span class="font-weight-bold text-uppercase">{{ $project->version}}</span>
+                                    Version:
+                                    <span class="font-weight-bold text-uppercase">{{ $project->version }}</span>
                                 </div>
                                 {{-- <div class="view-box bg-white rounded p-3 w-25 col-md-3 m-1" style="font-size: 18px">
                                     Downloads: 
                                     <span class="font-weight-bold">{{ $project->downloads }}</span>
                                 </div> --}}
                                 <div class="view-box bg-white rounded p-3 w-25 col-md-3 m-1" style="font-size: 18px">
-                                    Amount: 
-                                  <span class="font-weight-bold">{{ number_format((int) $project->amount) }}</span>
+                                    Amount:
+                                    <span class="font-weight-bold">{{ number_format((int) $project->amount) }}</span>
                                 </div>
-                                
+
                             </div>
-                            <div class="view-box bg-primary rounded d-flex justify-content-center align-items-center  col-md-3 m-1" style="font-size: 18px">
-                               <a href="{{ $project->video_link }}" target="_blank" class="btn btn-primary">Live Demo</a>
+                            <div class="d-flex">
+                                <div class="view-box bg-primary rounded d-flex justify-content-center align-items-center  col-md-3 m-1"
+                                    style="font-size: 18px">
+                                    <a href="{{ $project->video_link }}" target="_blank" class="btn btn-primary">Live
+                                        Demo</a>
+                                </div>
+                                <div class="view-box bg-dark rounded d-flex justify-content-center align-items-center  col-md-3 m-1"
+                                    style="font-size: 18px">
+                                    <a href="{{ $project->download_link }}" target="_blank" class="btn btn-dark">Downloads</a>
+                                </div>
                             </div>
-                           
+
                         </div>
                         <div class="col-md-12 py-3">
                             <h3 class="font-weight-bold mb-3">Description:</h3>
